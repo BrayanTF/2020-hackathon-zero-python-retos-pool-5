411b3e9077975a6ea002e17c8e295947691402f3
Brayan <brayan.gonzalez.adeje@gmail.com>
2020-05-16 17:35:22 +0200

    Terminado

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,17 +7,30 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    player = player.lower()
+    ai = ai.lower()
+    if player == ai:
+        return "Empate!"
+    elif player == "piedra" and ai == "tijeras":
+        return "Ganaste!"
+    elif player == "tijeras" and ai == "papel":
+        return "Ganaste!"
+    elif player == "papel" and ai == "piedra":
+        return "Ganaste!"
+    else:
+        return "Perdiste!"
+        
 
 # Entry Point
 def Game():
-    #
-    #
+    print("********* Bienvenido a Piedra, Papel y Tijeras *********")
+    # 
+    # Entrada del usuario
+    player = options[int(input("Escoge una opcion 1:Piedra 2:Papel 3:Tijeras ")) - 1]
     
     #
-    #
-    
+    # Numero aleatorio de la ia
+    ai = options[randint(0, 2)]
     winner = quienGana(player, ai)
-
+    print("La IA selecciono " + ai)
     print(winner)
-

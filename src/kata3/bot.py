3041d7294cc383718115b94aeef3dccc0f170630
Brayan <brayan.gonzalez.adeje@gmail.com>
2020-05-16 19:01:28 +0200

    termiando test pasados

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,29 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    update.message.reply_text('Hola, Geeks!')
+    return "Hola, Geeks!"
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    update.message.reply_text('Ayudame!')
+    return "Ayudame!"
 
 def mayus(update, context):
         #
-        return ""
+        texto_usuario = str(context.args[0]).upper()
+        update.message.reply_text(texto_usuario)
+        return texto_usuario
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
+    texto = update.message.text
+    texto_alrevez = ""
+    for x in range(0, len(texto)):
+        texto_alrevez = texto_alrevez + texto[(len(texto) - 1) - x]
+    update.message.reply_text(texto_alrevez)
     #
-    return ""
+    return texto_alrevez
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -34,18 +43,18 @@ def error(update, context):
 def main():
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1140849073:AAHValGGrVPOQyBJv3smTc8gzI0ybCafTFM", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("mayus", mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
+    dp.add_handler(MessageHandler(Filters.text, alreves))
     
     # Y este espera al error
     dp.add_error_handler(error)

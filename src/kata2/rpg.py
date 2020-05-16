216adc25d3304b351b7b86f0f7f712e9ab073264
Brayan <brayan.gonzalez.adeje@gmail.com>
2020-05-16 18:01:57 +0200

    terminado

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,16 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
+    #Creamos una string con todos los caracteres para acceder aleatoriamente a ella 
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    token = ""
     #
-    #
+    # Recorremos hasta la longitud del token y en cada iteracion cogemos 1 caracter random
+    for x in range(passLen):
+        token += characters[random.randint(0, len(characters) - 1)]
     
     #
     #
 
-    return ""
+    return token
